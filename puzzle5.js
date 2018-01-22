b1f874ab22ca911f88304cf96e9366b91b12b80c
mitchwong2021 <34347774+mitchwong2021@users.noreply.github.com>
2018-01-21 18:09:01 -0800

    Create puzzle5.js

---
+++ puzzle5.js
@@ -0,0 +1,21 @@
+function move {
+
+  right 
+  right
+  var color = getColor
+
+    if (getColor == "blue") {
+      down
+    } else {
+      up
+    }
+
+}
+
+move
+move 
+move
+
+right
+right
+right
