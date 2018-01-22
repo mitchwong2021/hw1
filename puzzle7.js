9551636ecbcd29c18c15527b670c4235be333659
mitchwong2021 <34347774+mitchwong2021@users.noreply.github.com>
2018-01-21 18:33:43 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,31 @@
+function moveDown {
+
+  while (getColor != "red") {
+    down
+  }
+
+}
+
+function moveUp {
+
+  while (getColor != "red") {
+    up
+  }
+
+}
+
+
+moveDown
+right
+right
+moveUp
+right 
+right
+moveDown
+right
+right
+moveUp
+right 
+right
+moveDown
+right
