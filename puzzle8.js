617a4285342e5c843ca5755365cb257ddbb0f619
mitchwong2021 <34347774+mitchwong2021@users.noreply.github.com>
2018-01-21 18:40:12 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,25 @@
+function moveDown {
+
+  var i = 0
+  while (i < 7) {
+    down
+    i = i + 1
+  }
+
+}
+
+function moveUp {
+  var i = 0
+  while (i < 7) {
+    up
+    i = i + 1
+  }
+  
+}
+
+down 
+moveDown
+moveUp
+moveDown
+moveUp
+moveDown
